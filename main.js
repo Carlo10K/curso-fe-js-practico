2976dfb9d69c79a19b52f8dae81e0adb746402aa
Carlo10K <carlosortegadiaz50@gmail.com>
2022-11-02 09:13:03 -0600

    agregando el menu ham y media querys

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const navEmail = document.querySelector('.navbar-email');
+const hamIcon = document.querySelector('.menu');
 
 navEmail.addEventListener('click',function(){
     const menu =  document.querySelector('.desktop-menu');
         menu.classList.toggle('inactive');
+});
+
+hamIcon.addEventListener('click',function(){
+    const mobilemenu = document.querySelector('.mobile-menu');
+        mobilemenu.classList.toggle('inactive');
 });
